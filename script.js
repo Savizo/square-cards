50f8ecacf36b24254585c87b265453442be65513
Aleksandar Georgiev <75254327+Savizo@users.noreply.github.com>
2022-06-15 22:28:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementsByClassName("fa-brands fa-youtube")[0].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=8I5UCTlMa34&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=10");
+});
+document.getElementsByClassName("fa-brands fa-youtube")[1].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=wtqbOFYivxw");
+});
+document.getElementsByClassName("fa-brands fa-youtube")[2].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=H5tuwAO-_Kg");
+});
+document.getElementsByClassName("article")[0].addEventListener("click", function(){
+    window.open("https://iamcodefoxx.github.io/Weather-App/");
+});
+document.getElementsByClassName("article")[1].addEventListener("click", function(){
+    window.open("https://iamcodefoxx.github.io/Quiz-App/");
+});
+document.getElementsByClassName("article")[2].addEventListener("click", function(){
+    window.open("https://iamcodefoxx.github.io/Marvel-API/");
+});
